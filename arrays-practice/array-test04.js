1d0c27c868a71bf9f59a155db2d837ecf9b52fa2
marfo19 <104007118+marfo19@users.noreply.github.com>
2022-10-15 14:01:18 +0200

    Delete arrays-practice directory

--- arrays-practice/array-test04.js
+++
@@ -1,6 +0,0 @@
-/* 4. Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18,
-72, 14, 9]. */
-
-var myArray = [5, 15, -5, 20, 12, 18, 72, 14, 9];
-
-var arrayElement = myArray[0];
