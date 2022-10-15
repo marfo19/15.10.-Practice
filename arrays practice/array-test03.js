1a082f270d5e6c0bc4463d5f304af486517d036b
DESKTOP-I7Q5440\Marko <markofoljan90@gmail.com>
2022-10-15 16:22:47 +0200

    finished array tests 3 and 4

--- arrays practice/array-test03.js
+++ arrays practice/array-test03.js
@@ -1,1 +1,9 @@
 // 3. Print all negative elements from the array [2, -4, 5, -2, -11].
+
+var myArray = [2, -4, 5, -2, -11];
+
+myArray[0] < 0 ? console.log(myArray[0]) : console.log("");
+myArray[1] < 0 ? console.log(myArray[1]) : console.log("");
+myArray[2] < 0 ? console.log(myArray[2]) : console.log("");
+myArray[3] < 0 ? console.log(myArray[3]) : console.log("");
+myArray[4] < 0 ? console.log(myArray[4]) : console.log("");
--- arrays practice/array-test04.js
+++ arrays practice/array-test04.js
@@ -1,2 +1,14 @@
 /* 4. Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18,
 72, 14, 9]. */
+
+var a = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+a[0] % 3 === 0 ? console.log(a[0]) : console.log("");
+a[1] % 3 === 0 ? console.log(a[1]) : console.log("");
+a[2] % 3 === 0 ? console.log(a[2]) : console.log("");
+a[3] % 3 === 0 ? console.log(a[3]) : console.log("");
+a[4] % 3 === 0 ? console.log(a[4]) : console.log("");
+a[5] % 3 === 0 ? console.log(a[0]) : console.log("");
+a[6] % 3 === 0 ? console.log(a[0]) : console.log("");
+a[7] % 3 === 0 ? console.log(a[0]) : console.log("");
+a[8] % 3 === 0 ? console.log(a[0]) : console.log("");
